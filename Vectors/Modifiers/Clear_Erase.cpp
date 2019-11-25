79170d6e188012df6d3ff6e8e4b705f041bff289
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-25 12:26:39 +0530

    Erase in Vectors

---
+++ Vectors/Modifiers/Clear_Erase.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	vector<int> arr;
+	int i;
+	for(i=0;i<10;++i)
+	{
+		arr.push_back(i);
+	}
+	cout<<"Displaying current Vector"<<endl;
+	for(i=0;i<arr.size();i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	//arr.erase(2); does not work as no interger input takes address
+	arr.erase(arr.begin());
+	cout<<"Displaying current Vector"<<endl;
+	for(i=0;i<arr.size();i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
