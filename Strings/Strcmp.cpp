00231ff78d1aaed9dde5cb56692726cc07d1f97c
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-26 06:26:49 +0530

    Compare Strings

---
+++ Strings/Strcmp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s,s1;
+	getline(cin,s);
+	getline(cin,s1);
+	//compare
+	if(s1==s)
+	{
+		cout<<"Same";
+	}
+	else
+	{
+		cout<<"Different";
+	}
+}
